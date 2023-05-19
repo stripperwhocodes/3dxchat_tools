111297217be777894a3e66d64885e1f339e0c358
Bitch who Codes <want@my.email>
2023-05-19 00:37:30 -0400

    prepare gentext to support multiple fonts
    soda font is still the only supported, but now it's
    easy to add new fonts

--- gentext.py
+++ gentext.py
@@ -20,21 +20,33 @@ def place_group(x, y, z, group, y_anchor, z_anchor):
 parser = argparse.ArgumentParser(description = "generate world file with specified text")
 parser.add_argument("text", type=str, help="text to generate")
 parser.add_argument("-o", type=str, default="", help="output file")
+parser.add_argument("-f", type=str, default="soda", help="font name to use")
 args = parser.parse_args()
-font_filename = "soda_font_curated.world"
-font_y_anchor = 25.3633
-font_z_anchor = 26.8788
-font_letter_width = 0.5
-font_index = list(string.ascii_uppercase) + \
-    [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ] + \
-    list(string.ascii_lowercase) + \
-    [ ".", ",", "'", '"', "`", "!", "?", ":", ";", "/", "-", "[", "]" ]
-with open(font_filename) as f:
+
+fonts = {
+    "soda": {
+        "filename" : "soda_font_curated.world",
+        "y_anchor" : 25.3633,
+        "z_anchor" : 26.8788,
+        "letter_width" : 0.5,
+        "index" : list(string.ascii_uppercase) + \
+            [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ] + \
+            list(string.ascii_lowercase) + \
+            [ ".", ",", "'", '"', "`", "!", "?", ":", ";", "/", "-", "[", "]" ]
+    }
+}
+
+font = fonts.get(args.f)
+if font is None:
+    print("unknown font")
+    exit(1)
+
+with open(font["filename"]) as f:
     font_data = json.load(f)
 # extract only letter shapes
 font_data = [ g["objects"] for g in font_data["objects"] if g["n"] == "group" ]
 font_data = sorted(font_data, key=lambda letter : leftmost_x(letter))
-assert len(font_index)==len(font_data), f"{len(font_index)}!={len(font_data)}"
+assert len(font["index"])==len(font_data), "{}!={}".format(len(font["index"]),len(font_data))
 
 # create world template, with spawner only and one empty group
 world = {
@@ -53,16 +65,16 @@ world = {
 x_pos = 0
 for l in args.text:
     if l == ' ':
-        x_pos+=2*font_letter_width
+        x_pos+=2*font["letter_width"]
         continue
     try:
-        index = font_index.index(l)
+        index = font["index"].index(l)
     except:
         index = None
     if index is not None:
-        letter = place_group(x_pos, 2, 0, font_data[index], font_y_anchor, font_z_anchor)
+        letter = place_group(x_pos, 2, 0, font_data[index], font["y_anchor"], font["z_anchor"])
         world["objects"].append(letter)
-    x_pos+=font_letter_width
+    x_pos+=font["letter_width"]
 if not args.o:
     print(world)
 else:
